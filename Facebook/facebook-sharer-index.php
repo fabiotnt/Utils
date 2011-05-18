ab433827aa9126002c93131c1313af16899fcd77
FabioTNT <fabio@fabiotnt.com>
2011-05-17 23:44:31 -0300

    Face Book Share Index HTML

---
+++ Facebook/facebook-sharer-index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt-br" dir="ltr" id="home">
+	<head>
+		
+		<!-- TITLE -->
+		<title>Your Site Name</title>
+		
+		<!-- META -->
+		<meta charset="utf-8" />
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<meta name="robots" content="ALL" />
+		<meta name="keywords" content="" />
+		<meta name="author" content="" />
+		<meta name="title" content="" />
+		<meta name="description" content="" />
+		<meta name="copyright" content="&copy; 2011 Banana Groove Studios®. All Rights Reserved" />
+		
+		<!-- Facebook Sharing -->
+		<meta property="og:title" content="" />
+		<meta property="og:description" content="" />
+		<meta property="og:type" content="website"> <!-- You can use: activity,sport,bar,company,cafe,hotel,restaurant,Groups,cause,sports_league,sports_team,Organizations,band,government,non_profit,school,university,actor,athlete,author,director,musician,politician,public_figure,city,country,landmark,state_province,album,book,drink,food,game,product,song,movie,tv_show,blog,website,article -->
+		<meta property="og:url" content="">
+		<meta property="og:site_name" content="">
+		<meta property="fb:admins" content="USER_ID" />
+		<meta property="og:image" content="">
+		
+		<!-- If you need to redirect to another page -->
+		<!-- <meta http-equiv="refresh" content="0;URL=http://www.yahoo.com.br/"> -->
+		
+		<!-- The thumb image and favorite icon -->
+		<link rel="image_src" href="" />
+		<link rel="shortcut icon" href="">
+		
+	</head>
+	<body></body>
+</html>
+
+
